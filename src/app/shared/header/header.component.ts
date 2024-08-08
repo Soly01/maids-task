71fd5dac291aa763f9f7adee3b3834e0af4815de
Soly01 <solimana425@yahoo.com>
2024-08-08 22:02:01 +0300

    adding badges for themes

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonModule, AvatarModule],
+  imports: [ButtonModule, AvatarModule, BadgeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
